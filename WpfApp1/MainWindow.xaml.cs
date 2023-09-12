60af6370b90e2e2fb4b05c117c733e7799367abf
student <student@s218-pc10>
2023-09-12 08:09:52 +0200

    Dodaj pliki projektów.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            Regex regex = new Regex("([0-9]{1,})");
+
+            if (Bok.Text != "" && regex.IsMatch(Bok.Text))
+            {
+                int a = Int32.Parse(Bok.Text);
+                int P = a * a;
+
+                Wynik.Content = "Pole kwadratu o boku " + a + " wynosi: " + P;
+            }
+
+            else
+            {
+                MessageBox.Show("Wpisz poprawne dane", "Komunikat", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
